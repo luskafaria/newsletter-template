0d40e4564bff8d5210e68d460fb2e2d13ecc9b64
Lucas Faria <lucas.faria.ext@adevinta.com>
2025-07-16 10:55:57 -0300

    feat: infra db

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,33 @@
 import { Client } from "pg";
 
-async function query(queryObject) {
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    max_connections: 500,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSSLValues(),
   });
 
+  await client.connect();
+  return client;
+}
+
+async function query(queryObject) {
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -25,4 +40,5 @@ async function query(queryObject) {
 
 export default {
   query,
+  getNewClient,
 };
